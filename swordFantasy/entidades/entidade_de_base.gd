a2dfd4fd58746138eafb91bcebbf367a7b90124a
Elder Campos <117109983+eldercamposds@users.noreply.github.com>
2025-02-19 13:20:38 -0300

    movimentação animais

---
+++ swordFantasy/entidades/entidade_de_base.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+var direcao: Vector2
+
+@export var _tempo_de_caminhada: Timer
+@export var _velocidade_de_movimento_normal: float = 32.0
+@export var _velocidade_de_movimento_correndo: float = 64.0
+
+func _ready() -> void:
+	direcao = retornar_direcao_aleatoria()
+	_tempo_de_caminhada.start(5.0)
+
+func _physics_process(_delta: float) -> void:
+	velocity = _velocidade_de_movimento_normal * direcao
+	move_and_slide()
+	
+	_bounce()
+	
+func _bounce() -> void:
+	if get_slide_collision_count() > 0:
+		direcao = velocity.bounce(get_slide_collision(0).get_normal()).normalized()
+	
+func retornar_direcao_aleatoria() -> Vector2:
+	return Vector2(
+		[-1, 0, +1].pick_random(),
+		[-1, 0, +1].pick_random()
+	).normalized()
+
+
+func _on_tempo_de_caminhada_timeout() -> void:
+	_tempo_de_caminhada.start(5.0)
+	if direcao != Vector2(0, 0):
+		direcao = Vector2(0, 0)
+		return
+	
+	if direcao == Vector2(0,0):
+		direcao = retornar_direcao_aleatoria()
+	
