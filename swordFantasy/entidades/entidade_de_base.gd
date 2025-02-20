9f2ff56e3f02ab6759d9e33113a93db9bc593e02
Elder Campos <117109983+eldercamposds@users.noreply.github.com>
2025-02-19 22:21:39 -0300

    Bug de movimentação dos animais

--- swordFantasy/entidades/entidade_de_base.gd
+++ swordFantasy/entidades/entidade_de_base.gd
@@ -9,30 +9,30 @@ var direcao: Vector2
 func _ready() -> void:
 	direcao = retornar_direcao_aleatoria()
 	_tempo_de_caminhada.start(5.0)
-
-func _physics_process(_delta: float) -> void:
+	
+func _physics_process(delta: float) -> void:
 	velocity = _velocidade_de_movimento_normal * direcao
 	move_and_slide()
 	
 	_bounce()
-	
+
 func _bounce() -> void:
 	if get_slide_collision_count() > 0:
 		direcao = velocity.bounce(get_slide_collision(0).get_normal()).normalized()
-	
+		
+
 func retornar_direcao_aleatoria() -> Vector2:
 	return Vector2(
 		[-1, 0, +1].pick_random(),
-		[-1, 0, +1].pick_random()
+		[1, 0, +1].pick_random()
 	).normalized()
-
-
+	
 func _on_tempo_de_caminhada_timeout() -> void:
 	_tempo_de_caminhada.start(5.0)
-	if direcao != Vector2(0, 0):
-		direcao = Vector2(0, 0)
+	if direcao != Vector2(0,0):
+		direcao = Vector2(0,0)
 		return
 	
 	if direcao == Vector2(0,0):
-		direcao = retornar_direcao_aleatoria()
+		direcao = retornar_direcao_aleatoria()	
 	
