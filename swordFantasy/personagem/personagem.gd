fc437daac4fdc0331c8a4cc2125a4c61a005e9e3
Elder Campos <117109983+eldercamposds@users.noreply.github.com>
2025-02-22 19:04:23 -0300

    Func Perdendo Vida
    Atualizando função de perda de vida

--- swordFantasy/personagem/personagem.gd
+++ swordFantasy/personagem/personagem.gd
@@ -83,7 +83,7 @@ func _on_temporizador_de_acoes_timeout() -> void:
 
 func _on_area_de_atque_area_entered(_area: Area2D) -> void:
 	if _area.is_in_group("area_de_dano"):
-		print(_area.get_parent().name)
+		_area.get_parent().perdendo_vida(randi_range(1,5))
 		return
 	
 	#if _area.is_in_group("area_de_dano"):
